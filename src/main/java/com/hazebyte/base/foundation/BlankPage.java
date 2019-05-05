319bf9209b032b1312f8d9e15bc8e896164648e7
imWillX <imWillX@users.noreply.github.com>
2019-05-05 00:32:36 -0700

    Add BlankPage

---
+++ src/main/java/com/hazebyte/base/foundation/BlankPage.java
@@ -0,0 +1,11 @@
+package com.hazebyte.base.foundation;
+
+import com.hazebyte.base.Base;
+import com.hazebyte.base.Size;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class BlankPage extends Base {
+    public BlankPage(JavaPlugin plugin, String title, Size size) {
+        super(plugin, title, size);
+    }
+}
