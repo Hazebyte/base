7b699ab9268b5e2f6a8cc1839ba0969eff6ad055
imWillX <imWillX@users.noreply.github.com>
2019-05-04 20:33:42 -0700

    Update Lib debug

--- src/main/java/com/hazebyte/base/util/Lib.java
+++ src/main/java/com/hazebyte/base/util/Lib.java
@@ -7,9 +7,11 @@ import java.util.logging.Logger;
  */
 public class Lib {
 
+    private static boolean DEBUG = false;
     private static Logger logger = Logger.getLogger("Base");
 
     public static void debug(Object... messages) {
+        if (!DEBUG) return;
         for (Object object : messages) {
             logger.info(object.toString());
         }
