831663188f0220fb722764d15e1e6464ed5cdaae
imWillX <imWillX@users.noreply.github.com>
2019-06-23 20:08:02 -0700

    Add common used variables

---
+++ src/main/java/com/hazebyte/base/Var.java
@@ -0,0 +1,12 @@
+package com.hazebyte.base;
+
+public interface Var {
+
+    int MAX_ROW = 6;
+    int ITEMS_PER_ROW = 9;
+    int MAX_ITEMS = MAX_ROW * ITEMS_PER_ROW;
+
+    int RIGHT_CENTER = 4;
+    int FROM_CENTER = 5;
+    int LEFT_CENTER = 6;
+}
