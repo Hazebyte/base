0417daa83965043ba4b1860d1e0d4a432738dec2
Charlie J <47271716+Chazza@users.noreply.github.com>
2019-07-28 23:51:30 +0100

    Update src/main/java/com/hazebyte/base/Base.java
    Co-Authored-By: William <imWillX@users.noreply.github.com>

--- src/main/java/com/hazebyte/base/Base.java
+++ src/main/java/com/hazebyte/base/Base.java
@@ -361,7 +361,7 @@ public abstract class Base extends Component implements InventoryHolder {
      * @param event
      */
     public void onInventoryClick(InventoryClickEvent event) {
-        if(event.getClickedInventory() == null) return;
+        if (event.getClickedInventory() == null) return;
         if(event.getRawSlot() < 0) return;
         int slot = event.getRawSlot();
         int page = this.getProperty(event.getWhoClicked().getUniqueId().toString(), 0);
